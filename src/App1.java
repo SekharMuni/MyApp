164a442fd838290a64b29573a17a6c5bb17ae62f
Munisekhar Gunapati <gunapatimunisekhar@gmail.com>
2017-12-23 14:23:41 +0530

    Update App1.java
    2nd modification

--- src/App1.java
+++ src/App1.java
@@ -4,6 +4,8 @@ public class App1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World...");
 		System.out.println("Hello World..pull");
+		System.out.println("Hello World.12345");
+		
 	}
 
 }
